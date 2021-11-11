1fc7d73231a4eb9d73a0d714dad34d63a0296670
Naava Kauffman <naava456@gmail.com>
2021-11-11 17:46:01 +0200

    Discard Pile (version 1.0)

---
+++ Uno/Uno/Deck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno
+{
+    class Deck
+    {
+        public List<Card> Pile { get; set; }
+
+        public Deck()
+        {
+            Pile = new List<Card>();
+        }
+    }
+}
