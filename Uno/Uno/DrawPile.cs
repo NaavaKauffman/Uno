fa71086aa38cae31e785dce8b185e589daf58869
Chani Wachsstock <chaniwachsstock@gmail.com>
2021-11-14 01:08:10 +0200

    Add Draw Pile and BuildDeck method

---
+++ Uno/Uno/DrawPile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno
+{
+    class DrawPile : Deck
+    {
+        public DrawPile() : base()
+        {
+            Pile = BuildDeck();
+        }
+
+        public List<Card> BuildDeck()
+        {
+            List<Card> deck = new List<Card>();
+            int cardCounter = 0;
+            for (int color = 0; color < NUMBEROFCOLORS; color++)
+            {
+                for (int cardNumber = 1; cardNumber <= 9; cardNumber++) //
+                {
+                    Card card1 = new Card(color, cardNumber); //creates cards with number and color
+
+                    deck.Add(card1);
+
+                    cardCounter++;
+                }
+            }
+
+            return deck;
+        }
+    }
+}
