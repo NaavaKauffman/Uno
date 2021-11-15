0290152f24d09018be939926931b56360f7643a7
Chani Wachsstock <chaniwachsstock@gmail.com>
2021-11-15 09:53:46 +0200

    Building Decks

--- Uno/Uno/Card.cs
+++ Uno/Uno/Card.cs
@@ -6,20 +6,22 @@ using System.Threading.Tasks;
 
 namespace Uno
 {
-    class Card
+    public class Card
     {
         public int CardNum { get; set; } //Don't worry, whoever writes the real class will
                                          //do something more normal. This is just for testing purposes.
-
-        public Card(int cardnum)
+        public int Color { get; set; }
+        public int CardIndex { get; set; } // Needed in order to find the correct card
+        public Card(int cardnum, int color)
         {
             CardNum = cardnum;
+            Color = color; //this really should be an enum but I need something to test
         }
 
         //testing, testing, 123
         public override string ToString()
         {
-            return $"{CardNum}";//practice comment
+            return $"{CardNum} {Color}";//practice comment
         }
     }
 }
